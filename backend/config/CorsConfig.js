29c69920bbb2a962810519451ea62d4360af5308
Kelvyn <KelvynHesley@github.com>
2025-11-08 16:04:13 -0300

    fix(backend): Corrige o array de origens do CORS

--- backend/config/CorsConfig.js
+++ backend/config/CorsConfig.js
@@ -1,11 +1,13 @@
 const cors = require('cors');
 
 const corsOptions = {
-    origin: 'https://espike-frontend.onrender.com',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: '*',
-    credentials: true
+    origin: [
+        'http://localhost:8081',
+        'https://espike-frontend.onrender.com' 
+    ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: '*',
+    credentials: true
 };
 
 module.exports = cors(corsOptions);
-//trabalhar o server/app js
